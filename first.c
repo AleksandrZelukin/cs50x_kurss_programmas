2a3038dbd3b3413cc982b12088609d893871532f
Aleksandr <alexandr44@inbox.lv>
2022-09-04 17:22:04 +0300

    Add date

--- first.c
+++ first.c
@@ -1,7 +1,20 @@
+
 #include <stdio.h>
 
-int main ()
-{
+int main (void)
 
-    printf("Sveiki!");
+{
+    int  a,b,c,d,e;
+    a = scanf("%d\n", &a);
+    //a = 47;
+    b = a / 25;
+    c = (a - b * 25)/10;
+    d = (a - b * 25 - c *10)/5;
+    e = (a - b * 25 - c *10 - d * 5)/1;
+    printf("Quarter %d\n", b );
+    printf("Disme %d\n", c);
+    printf("Nickel %d\n", d );
+    printf("Penny %d\n", e );
+     
+
 }
