36460bd22918e4e4c0ae5752d4795d77573a68cd
AleksandrZelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2022-09-29 08:53:03 +0300

    Create parametrs end

---
+++ parametr_end.py
@@ -0,0 +1,10 @@
+print("Hello friends how are you?", end = ' ')
+print("I am fine!", end ='#')
+print() # prints new line
+
+print("ABC", end='')
+print("PQR", end='\n') # ends with a new line
+
+print("This is line 1.", end='[END]\n')
+print("This is line 2.", end='[END]\n')
+print("This is line 3.", end='[END]\n')
